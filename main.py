face5c12753ba1e6fc202078708c948f04834564
winner23456@naver.com <winner23456@naver.com>
2024-04-17 02:55:00 +0000

    틀만 맞춤 중간 코드는 추가해야 한다.

---
+++ main.py
@@ -0,0 +1,14 @@
+# 사용자가 chatbot을 사용하는 함수 
+def main():
+    pass
+
+    # chat 함수 - User Input과 LLM Answer를 어떻게 시스템 설정을 하는가에 대한 함수 (구현 해야 합니다!)
+    
+    # retriever 함수 - User Input을 바탕으로 Vector DB에서 가장 근접한 대답을 찾는 함수 (구현 해야 합니다!)
+    
+    # gemini 함수 - User Input과 retriver 함수에서 반환한 답변을 바탕으로 Gemini한테 call 하는 함수 (구현 해야 합니다!)
+    
+    # logger 함수 - User Input과 gemini 함수에서 반환한 최종 답변을 바탕으로 로그를 찍는 함수  (구현 해야 합니다!)
+
+if __name__ == '__main__':
+    main()
---
+++ storage.py
@@ -0,0 +1,10 @@
+# 사전에 채팅 히스토리를 기록
+# 여러 데이터를 수집해서 embedding(벡터화) 하고 Vector DB에 저장한다.
+def storage():
+    chat_history = []
+    
+    # save_vecotor_db 함수 : URL에 있는 웹페이지 30개(??)를 수집해서 embedding(벡터화) 하고 Vector DB에 저장하는 함수 
+
+
+if __name__ == '__main__':
+    storage()
